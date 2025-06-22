e866a9e142deae08cb7ea62fbe35de07317f7fa8
Hector Zurga <hjzurga@gmail.com>
2025-06-21 20:30:44 -0500

    feat: initial view

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		t, err := template.ParseFiles("templates/index.html")
+
+		if err != nil {
+			c.String(500, "Error parsing template")
+			return
+		}
+
+		c.Status(http.StatusOK)
+		t.Execute(c.Writer, nil)
+	})
+	router.Run(":8000")
 }
