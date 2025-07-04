de141788cec08531a71da7176c57bb27651edca2
Hector Zurga <hjzurga@gmail.com>
2025-07-04 17:51:57 -0500

    removing group

--- modules/shortener/routes.go
+++ modules/shortener/routes.go
@@ -9,8 +9,7 @@ type ShortenerRoutes struct{}
 func (sr *ShortenerRoutes) LoadRoutes(server *gin.Engine) {
 	controller := &Controller{}
 
-	group := server.Group("/shortener")
-	group.GET("/", controller.IndexView)
-	group.POST("/shorten", controller.ShortenURL)
-	group.GET("/x/:shortURL", controller.RedirectURL)
+	server.GET("/", controller.IndexView)
+	server.POST("/shorten", controller.ShortenURL)
+	server.GET("/x/:shortURL", controller.RedirectURL)
 }
